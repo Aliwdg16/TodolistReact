89db6349181116d32253f9bd2761187769df9edc
aliwdg16 <alialtoto@hotmail.com>
2024-04-23 17:19:50 +0200

    Worken version

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Edit } from './Components/Edit'
-import { Clearlist } from './Components/Clearlist'
-import { Add } from './Components/Add'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Edit } from "./Components/Edit";
+import { Clearlist } from "./Components/Clearlist";
+import { Add } from "./Components/Add";
+import { SetLocalStorage } from "./Components/SetLocalStorage";
+import { GetlocalStorage } from "./Components/GetlocalStorage";
 
 function App() {
-
   return (
     <>
-    <div className='container' >
-
-    <header>
+      <div className="container">
+        <header>
           <div className="headertodo">
-            <div className="logona"> <img
-              src="https://static.vecteezy.com/system/resources/previews/006/549/765/non_2x/to-do-list-hand-drawn-doodle-icon-free-vector.jpg"
-              className="imglogo"
-              alt="logo"
-            /> <h1> To-do-list </h1></div>
-            <div className="headercontant">  
-              <p >Überuns</p>
-              <p >Kontakt</p>  
-          </div>
+            <div className="logona">
+              {" "}
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/006/549/765/non_2x/to-do-list-hand-drawn-doodle-icon-free-vector.jpg"
+                className="imglogo"
+                alt="logo"
+              />{" "}
+              <h1> To-do-list </h1>
+            </div>
+            <div className="headercontant">
+              <p>Überuns</p>
+              <p>Kontakt</p>
+            </div>
           </div>
         </header>
         <main>
@@ -35,60 +39,39 @@ function App() {
                 alt="logo"
               />
               <h1>To-Do Liste</h1>
-              
             </div>
             <div className="inputbtn">
-              <input type="text" className="add" id="add" placeholder="Enter task" />
+              <input
+                type="text"
+                className="add"
+                id="add"
+                placeholder="Enter task"
+              />
 
-              <Add/>
-            
+              <Add />
             </div>
             <div className="btnlist">
+              <Clearlist />
+              <Edit />
+               {/* <SetLocalStorage /> */}
+              {/* <GetlocalStorage />  */}
+            </div> 
 
-             <Clearlist/>
-               <Edit/>
-  
-            </div>
-          
             <div className="ulli">
               <ul className="list-group list-group-flush" id="itemlist">
-                {/* <li className="list-group-item">ali</li>
+                <li className="list-group-item">ali</li>
                 <li className="list-group-item">Daniel</li>
                 <li className="list-group-item">clara</li>
-                <li className="list-group-item">michal</li>  */}
+                <li className="list-group-item">michal</li> 
               </ul>
             </div>
           </div>
         </main>
-      
       </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-{/* 
-      <Edit/> */}
-    
-   
     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Edit.jsx
+++ src/Components/Edit.jsx
@@ -1,10 +1,35 @@
-
+// import { SetLocalStorage } from "./SetLocalStorage";
+import { useEffect, useState } from "react"
 
 export const Edit= () => {
+
+  function Edit_button(){
+  const [edit,useEdit]=useState([])
+
+  edit =document.querySelector("list-group-item")
+  
+  const editnewitem=useEffect()
+if(edit)
+{
+  editnewitem = prompt("Edit task:",edit.textContent);
+  if(editnewitem!==null){
+edit.textContent=useEffect;
+
+  }
+
+} 
+ else {
+  alert("Please select a task to edit.");
+}
+
+}
   return (
     <>
+    
+<button className="btnadditem2" id="Edit_button" onClick={Edit_button}>Edit</button> 
 
-<button className="btnadditem2" id="Edit_button">Edit</button> 
+
+{/* <SetLocalStorage/> */}
 
     </>
   )
